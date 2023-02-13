98406987f6c2a3af931e6b30fb589ad97594767c
Levente Orban <levente.orban@dyrector.io>
2023-02-13 08:09:28 +0100

    feat: add tailwind to the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<div class="text-xl text-neutral-400">
+    <h1>Welcome to CloudNative.hu</h1>
+    <p>Coming soon...</p>
+</div>
+
